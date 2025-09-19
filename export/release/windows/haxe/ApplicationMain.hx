2b5d42b5feef62683b2c45f8a967f1af1dcbd659
Damightyq <damightyq10@gmail.com>
2025-09-18 20:30:51 -0400

    oh boy HERE COMES THE OVERHAUL!!!!

--- export/release/windows/haxe/ApplicationMain.hx
+++ export/release/windows/haxe/ApplicationMain.hx
@@ -35,7 +35,7 @@ class ApplicationMain
 		ManifestResources.init(config);
 		#end
 
-		app.meta["build"] = "10";
+		app.meta["build"] = "1";
 		app.meta["company"] = "ShadowMario";
 		app.meta["file"] = "PsychEngine";
 		app.meta["name"] = "Friday Night Funkin': Psych Engine";
